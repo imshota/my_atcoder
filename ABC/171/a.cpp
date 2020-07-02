1af97dc7638ebe2ca35cadf51a1771a3687802eb
imshota <imshotaseo@icloud.com>
2020-07-02 12:59:47 +0900

    ABC 171 a c++

---
+++ ABC/171/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   char s;
+   cin >> s;
+
+   if (isupper(s))
+   {
+       cout << "A" << endl;
+   }
+   else
+   {
+       cout << "a" << endl;
+   }
+
+   return 0;
+}
